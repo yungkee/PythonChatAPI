3567a5886da0500521f25d03a318859487ea5d42
Yungkee <lev-118@mail.ru>
2018-09-24 16:36:00 +0300

    auth and regist http request + html wrapper

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
 from source import create_app, socketio
+from flask import Flask, render_template, request, redirect, redirect, url_for, session, escape, flash
+from flask_socketio import SocketIO, emit
 
 app = create_app()
+
+@app.route('/')
+def mainpage(): 
+	if 'user' in session:
+		flash('You are successfully logged in!')
+		return render_template('index.html', login='True')
+	else:
+		flash('You need register account or login in if you want to use chat')
+		return render_template('index.html', login='False')
+
+@app.route('/register', methods=['GET', 'POST']) #regestration
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        return 'error'
+
+@app.route('/login', methods=['GET', 'POST']) #authorization
+def login():
+    if 'user' in session:
+        return redirect(url_for('mainpage'))
+    else:
+        if request.method == 'GET':
+            return render_template('login.html')
+
+        elif request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            session['user'] = username
+            return redirect(url_for('chat', user=escape(session['user'])))
+
+@app.route('/logout', methods =['POST'])
+def logout():
+	session.pop('user', None)
+	return redirect(url_for('mainpage'))
+
+@app.route('/chat/<user>')
+def chat(user):
+	if 'user' in session:
+		return render_template('client.html', user = escape(session['user']))
+	else:
+		return redirect(url_for('login'))
+
+def messageRecived():
+  print( 'message was received!!!' )
+
+@socketio.on( 'message event' )
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) )
+  socketio.emit( 'message_response', json, callback=messageRecived )
 
 if __name__ == '__main__':
 	socketio.run(app, debug=True, host='localhost', port=5000)
--- source/models.py
+++ source/models.py
@@ -1,15 +1,17 @@
+import sys
+import 
 from flask import Flask 
 
 #TODO
 class DataBase:
     def __init__(self, db):
-        pass
+        self.db = dev
+        self.con = r.
 
 class Message(DataBase):
     def __init__(self, db, table):
         super().__init__(db)
         self.table = table
-
 
 class ChatRoom(DataBase):
     def __init__(self, db, table):
@@ -18,6 +20,17 @@ class ChatRoom(DataBase):
         self.banned_users = set()
         self.room_list = set()
         self.users = set()
+
+    def add_user(self, user, room):
+        source.db(self.db).table(self.table).filter({'room':room, 'room_user':user}).delete().run(self.conn)
+
+    def user_list(self, room):
+        users = source.db(self.db).table(self.table).filter({'room': room}).run(self.conn)
+        user_list = []
+        for user in users:
+            user_list.append(user['room_user'])
+        user_count = len(user_list)
+        return user_list, user_count
 
 #TODO
 class User(DataBase):
